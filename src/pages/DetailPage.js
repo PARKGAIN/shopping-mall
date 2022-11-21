54dc24e3b50092955629f5b9206f5d278f266dc7
PARK GA IN <84880886+PARKGAIN@users.noreply.github.com>
2022-11-22 00:45:15 +0900

    feat: useEffect 로 2초뒤에 컴포넌트 없애는 것 하는 중

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,29 +1,63 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import data from '../data';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import data from "../data";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: ${(props) => props.bg};
+  color: ${(props) => (props.bg == "blue" ? "white" : "black")};
+  padding: 10px;
+`;
+let newBtn = styled.button(YellowBtn);
 
 function DetailPage(props) {
-  let {id}=useParams();
+  let { id } = useParams();
   // console.log(props.shoes.map(x=>x.id));
-  let 찾은상품 = props.shoes.find(function(x){
-    return x.id == id
+  let 찾은상품 = props.shoes.find(function (x) {
+    return x.id == id;
+  });
+  useEffect(() => {
+    setTimeout(() => {
+      document.removeEventListener("click", <Discount />);
+    }, 2000);
   });
 
+  let [count, setCount] = useState(0);
   return (
     <div className="container">
-     <div className="row">
-    <div className="col-md-6">
-      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+      <Discount />
+      {count}
+      <YellowBtn
+        bg="blue"
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </YellowBtn>
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
     </div>
-    <div className="col-md-6">
-      <h4 className="pt-5">{찾은상품.title}</h4>
-      <p>{찾은상품.content}</p>
-      <p>{찾은상품.price}</p>
-      <button className="btn btn-danger">주문하기</button> 
-    </div>
-  </div>
-</div> 
-  )
+  );
 }
 
-export default DetailPage
+function Discount() {
+  return (
+    <>
+      <div className="alert alert-warning">2초 이내 구매시 할인</div>
+    </>
+  );
+}
+export default DetailPage;
